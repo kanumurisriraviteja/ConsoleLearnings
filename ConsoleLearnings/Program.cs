0534caea315b52ecfa291ff305d3e783039b0353
Kanumuri SriRaviTeja <Kanumuri SriRaviTeja@KANUMURISRIRAVI>
2018-01-08 22:48:25 +0530

    Initial

--- ConsoleLearnings/Program.cs
+++ ConsoleLearnings/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleLearnings
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Program");
+            Console.ReadKey();
         }
     }
 }
