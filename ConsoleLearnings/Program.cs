cfd2163bc6a16ebcca6b424d6ac13723f13af0e1
Kanumuri SriRaviTeja <Kanumuri SriRaviTeja@KANUMURISRIRAVI>
2018-05-17 08:39:02 +0530

    docs

--- ConsoleLearnings/Program.cs
+++ ConsoleLearnings/Program.cs
@@ -4,18 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleLearnings
+
+public class Program
 {
-    class Program
+
+    public static bool Check_sub(String str1, String str3)
     {
-        static void Main(string[] args)
+        if (str3.Contains(str1))
         {
-            //LCollections.MainCollections();
-            //L1.DelLearnings();
-            //Fileoperations.operationsFile();
-            //CPrograms.Programs();
-            Console.WriteLine("Program");
-            Console.ReadKey();
+            return true;
         }
+        return false;
+    }
+
+    static void Main(string[] args)
+    {
+        String str1 = "waterbottle";
+        String str2 = "erbottlewat";
+        String str3 = str1 + str1;
+
+        if (str1.Length != str2.Length)
+        {
+            Console.WriteLine("not rotation");
+        }
+        else
+        {
+            if (Check_sub(str2, str3))
+            {
+                Console.WriteLine(" rotation");
+            }
+            else
+            {
+                Console.WriteLine("not rotationn");
+            }
+        }
+        Console.ReadKey();
     }
 }
